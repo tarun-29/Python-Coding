9c0a4b51048f7ee1fbb580bee0a7fe0194cf3d68
Tarun Kantiwal <tarunkantiwal@Taruns-MacBook-Pro.local>
2020-07-07 13:14:11 +0530

    Number Operator Comments

---
+++ Number Operator Comments/comments.py
@@ -0,0 +1,4 @@
+print(1/3) #divison returns float even when dividing two integers
+print(2**3) # ** is exponent and answer will be 8
+print(5%3) #module answer will be remainder so 2
+print(5//3) #return interger answer so answer will be 1
