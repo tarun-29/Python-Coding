37ad99ffb0f24235d53fe32c8e92e18e6182c1aa
Tarun Kantiwal <tarunkantiwal@Taruns-MacBook-Pro.local>
2020-07-07 18:01:26 +0530

    Boolean and conditional exercise

---
+++ Boolean Conditional Logic/demo.py
@@ -0,0 +1,36 @@
+# name = "Arya Stark"
+
+# if name == "Arya Stark" :
+#     print("Valar Morghulis")
+
+# elif name == "Jon Snow" :
+#     print("You know nothing")
+
+# else :
+#     print("Carry On")
+
+# city = input("Where do you live")
+
+# if city == "los angeles" or city == "san fransico" :
+#     print("you live in california!")
+
+# else : 
+#     print("You live somewhere else")
+
+# exercise of boolean section
+
+# ask for age 
+
+age = input("Please enter your age")
+age = int(age)
+if(21>age>=18) :
+    print("Wristeband")
+
+elif (age>=21):
+    print("Drink Normal Entry")
+
+elif :
+    print("Too Young, Sorry")
+
+else :
+    print("insert your age")
