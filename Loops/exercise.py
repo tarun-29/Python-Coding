c8833c59555307d7210923fb46bea83829abd3c7
Tarun Kantiwal <tarunkantiwal@Taruns-MacBook-Pro.local>
2020-07-08 15:07:43 +0530

    For and While

---
+++ Loops/exercise.py
@@ -0,0 +1,23 @@
+#for loop exercise
+
+
+# for num in range(1,21): 
+#     if num==4 or num==14:
+#         print(f"{num} is unlucky")
+#     elif num%2==0 :
+#         print(f"{num} is even")
+#     else : 
+#         print(f"{num} is odd")
+
+
+#while loop exercise 
+# num = 1
+# while num<=9 :
+#     print(("\U0001f600")*int(num))
+#     num = num+1
+
+
+ans = input("Hey how it's going ?")
+while ans != "stop copying me" :
+    print(ans)
+    ans = input()
---
+++ Loops/range.py
@@ -0,0 +1,19 @@
+# for num in range(10,20,2):
+#     print(num)
+
+# r = range(10)
+# print(r) #it will print range(10)
+# print(list(r))#it will print [0,1,2,3,4,5....,9]
+
+# nums = range(1,10,2)
+# print(nums)
+# print(list(nums))#it will print odd between 1,10
+
+# ans = input("How many i have to tell you")
+# ans  = int(ans)
+
+# for num in range(ans):
+#     print(f"{num+1} :Clean up your room")
+
+
+#while loop
