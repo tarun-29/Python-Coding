eed231b244bb9c1105231ba7689ff1033aa2d3e2
Tarun Kantiwal <tarunkantiwal@Taruns-MacBook-Pro.local>
2020-07-08 21:46:44 +0530

    Lists

---
+++ Lists/forLoop.py
@@ -0,0 +1,11 @@
+colors = ["purple", "teal", "magenta", "crimson", "blue"]
+index = 0
+index = int(index)
+
+# for color in colors : 
+#     print(color)
+
+
+while index < len(colors) :
+    print(colors[index])
+    index = index +1
---
+++ Lists/listComp.py
@@ -0,0 +1,6 @@
+# nums = [1,2,3]
+# 
+# print([x*10 for x in nums])
+#[10,20,30]
+
+#comprehension  with conditional logic 
