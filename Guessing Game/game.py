8047a350bed191e05ef43699834e6d5c6c8cc4f3
Tarun Kantiwal <tarunkantiwal@Taruns-MacBook-Pro.local>
2020-07-08 19:32:49 +0530

    Guessing Game quit condition

--- Guessing Game/game.py
+++ Guessing Game/game.py
@@ -2,7 +2,7 @@ import random
 
 randnum = random.randint(1,10);
 guess = None
-while randnum != guess :
+while 1 :
     guess = input("Put one number between 1 to 10")
     guess = int(guess)
     if(guess<randnum):
@@ -11,5 +11,13 @@ while randnum != guess :
         print("Too High\n")
     else :
         print("You Won\n")
+        playAgain = input("If you want to play again press y otherwise n")
+        if playAgain == 'y' :
+            randnum = random.randint(1,10);
+            guess = None
+        else: 
+            print("Thank You for playing")
+            break
+            
     
 print(randnum)
